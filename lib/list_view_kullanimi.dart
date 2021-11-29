9464e11275622055d7159bc8f36641dfab6bce61
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2021-11-29 12:32:18 +0300

    project updated
    listview kullanımı ve hazır paketlerin projeye dahil edilmesi

--- lib/list_view_kullanimi.dart
+++ lib/list_view_kullanimi.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class ListViewKullanimi extends StatelessWidget {
   ListViewKullanimi({Key? key}) : super(key: key);
@@ -22,34 +23,48 @@ class ListViewKullanimi extends StatelessWidget {
 
   ListView buildListviewSeperated() {
     return ListView.separated(
-        itemBuilder: (BuildContext context, int index) {
-          var oAnKiOgrenci = tumOgrenciler[index];
-          var oAnKiIndex = index + 1;
-          return Card(
-            color:
-                index % 2 == 0 ? Colors.grey.shade500 : Colors.grey.shade300,
-            child: ListTile(
-              onTap: () {
-                print("Tıklanan öğrenci=>$oAnKiIndex");
-              },
-              title: Text(oAnKiOgrenci.isim),
-              subtitle: Text(oAnKiOgrenci.soyIsim),
-              leading: CircleAvatar(
-                child: Text(oAnKiOgrenci.id.toString()),
-              ),
+      itemBuilder: (BuildContext context, int index) {
+        var oAnKiOgrenci = tumOgrenciler[index];
+        var oAnKiIndex = index + 1;
+        return Card(
+          color: index % 2 == 0 ? Colors.grey.shade500 : Colors.grey.shade300,
+          child: ListTile(
+            onTap: () {
+              if (index % 2 == 0) {
+                EasyLoading.instance.backgroundColor = Colors.red[900];
+              } else {
+                EasyLoading.instance.backgroundColor = Colors.blue[900];
+                EasyLoading.instance.textColor = Colors.yellow;
+              }
+              EasyLoading.showToast(
+                "Tıklanın öğrenci => $oAnKiIndex",
+                duration: Duration(
+                  seconds: 3,
+                ),
+                toastPosition: EasyLoadingToastPosition.bottom,
+              );
+            },
+            onLongPress: () {
+              UseAlertDialog(context, oAnKiOgrenci);
+            },
+            title: Text(oAnKiOgrenci.isim),
+            subtitle: Text(oAnKiOgrenci.soyIsim),
+            leading: CircleAvatar(
+              child: Text(oAnKiOgrenci.id.toString()),
             ),
-          );
-        },
-        itemCount: tumOgrenciler.length,
-        separatorBuilder: (context, index) {
-          var oAnKiIndex = index + 1;
-          if (oAnKiIndex % 4 == 0) {
-            return Divider(thickness: 4, color: Colors.teal.shade400);
-          } else {
-            return Container();
-          }
-        },
-      );
+          ),
+        );
+      },
+      itemCount: tumOgrenciler.length,
+      separatorBuilder: (context, index) {
+        var oAnKiIndex = index + 1;
+        if (oAnKiIndex % 4 == 0) {
+          return Divider(thickness: 4, color: Colors.teal.shade400);
+        } else {
+          return Container();
+        }
+      },
+    );
   }
 
   ListView ClassicListView() {
@@ -68,10 +83,40 @@ class ListViewKullanimi extends StatelessWidget {
   }
 }
 
+void UseAlertDialog(BuildContext myContext, Ogrenci secilen) {
+  showDialog(
+      barrierDismissible: false,
+      context: myContext,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Öğrenci bilgisi"),
+          content: SingleChildScrollView(
+            child: Text(secilen.toString()),
+          ),
+          actions: [
+            ButtonBar(
+              children: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("KAPAT")),
+                TextButton(onPressed: () {}, child: Text("ANLADIM")),
+              ],
+            )
+          ],
+        );
+      });
+}
+
 class Ogrenci {
   final int id;
   final String isim;
   final String soyIsim;
 
   Ogrenci(this.id, this.isim, this.soyIsim);
+  @override
+  String toString() {
+    return ("Ogrenci adı : $isim, Soyisim : $soyIsim");
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_temel_widget/button_t%C3%BCrleri.dart';
-import 'package:flutter_temel_widget/card_listtile.dart';
-import 'package:flutter_temel_widget/image_widgets.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_temel_widget/list_view_kullanimi.dart';
-import 'package:flutter_temel_widget/my_counter_page.dart';
 
 void main() {
   runApp(MyApp());
+  configLoading();
+}
+
+void configLoading() {
+  EasyLoading.instance
+    ..displayDuration = const Duration(milliseconds: 2000)
+    ..indicatorType = EasyLoadingIndicatorType.fadingCircle
+    ..loadingStyle = EasyLoadingStyle.custom
+    ..indicatorSize = 45.0
+    ..radius = 10.0
+    ..progressColor = Colors.yellow
+    ..backgroundColor = Colors.black
+    ..indicatorColor = Colors.yellow
+    ..textColor = Colors.white
+    ..maskColor = Colors.blue.withOpacity(0.5)
+    ..userInteractions = true
+    ..dismissOnTap = false;
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "MY APP", home: ListViewKullanimi());
+    return MaterialApp(
+      title: "MY APP",
+      home: ListViewKullanimi(),
+      builder: EasyLoading.init(),
+    );
   }
 }
